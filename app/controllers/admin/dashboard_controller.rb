b75eb1792897af636a81e066c633764fac2a154a
davidclaveau <daveclaveau@gmail.com>
2021-06-23 15:48:57 -0700

    Add links to product and category pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
