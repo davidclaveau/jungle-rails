3d4a4c9214462dbc53bdb6f25e146865aa2dd62c
davidclaveau <daveclaveau@gmail.com>
2021-06-23 14:13:00 -0700

    Add AR query and render info

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,27 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+
+    line_items = LineItem.where(order_id: params[:id])
+
+    @products = LineItem.select(:image, :name, :quantity, :item_price_cents, :total_price_cents).joins(:product, :order).where(order_id: params[:id])
+
+    # @products = Product.select(Arel.star).where(LineItem.arel_table[:order_id].eq(params[:id])).joins(
+    #   Product.arel_table.join(LineItem.arel_table).on(
+    #     LineItem.arel_table[:product_id].eq(Product.arel_table[:id])
+    #   ).join_sources
+    # ).joins(
+    #   Product.arel_table.join(Order.arel_table).on(
+    #     LineItem.arel_table[:order_id].eq(Order.arel_table[:id])
+    #   ).join_sources
+    # )
+
+    # SELECT *
+    #   FROM products
+    #   JOIN line_items ON line_items.product_id = products.id
+    #   JOIN orders ON line_items.order_id = orders.id
+    #  WHERE line_items.order_id = 1;
+
   end
 
   def create
