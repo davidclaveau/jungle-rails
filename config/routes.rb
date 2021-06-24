ee26e60435613f23db4252bfc9d1eeb61e1cc3e8
davidclaveau <daveclaveau@gmail.com>
2021-06-24 16:15:49 -0700

    Add sales route, controller, model and views

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
+    resources :sales, only: [:index, :new]
   end
 
   resources :about, only: [:index]
