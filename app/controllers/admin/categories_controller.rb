3be6a3ff390b398fc69951716a1725b652cc8ab1
davidclaveau <daveclaveau@gmail.com>
2021-06-24 10:32:15 -0700

    Properly save new category when created

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,6 +9,21 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 
 end
