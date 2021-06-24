945bacdef39b23f29a20b1bf8db643b55521123e
davidclaveau <daveclaveau@gmail.com>
2021-06-24 16:46:23 -0700

    Add announcement to homepage for active sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,10 @@
 class Sale < ActiveRecord::Base
 
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?",
+           Date.current, Date.current)
+  end
+
   def finished?
     ends_on < Date.current
   end
