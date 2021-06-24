226b9bad0b1732505d6d1bb3b4b2f6be694e7685
davidclaveau <daveclaveau@gmail.com>
2021-06-24 10:14:56 -0700

    Render categories on the admin page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,12 +1,21 @@
 class Admin::CategoriesController < ApplicationController
 
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @categories = Category.new
   end
 
   def create
+    # @categories = Category.new(category_params)
+
+    # if @categories.save
+    #   redirect_to [:admin, :categories], notice: 'Category created!'
+    # else
+    #   render :new
+    # end
   end
 
 end
