385d5127a10c96850785223ccc63d9d8b6ad4356
davidclaveau <daveclaveau@gmail.com>
2021-06-24 10:31:10 -0700

    Create page and form for new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,17 +5,10 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
-    @categories = Category.new
+    @category = Category.new
   end
 
   def create
-    # @categories = Category.new(category_params)
-
-    # if @categories.save
-    #   redirect_to [:admin, :categories], notice: 'Category created!'
-    # else
-    #   render :new
-    # end
   end
 
 end
