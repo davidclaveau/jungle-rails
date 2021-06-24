fac41c43d0d1b038f83f22f33c4f4ea5681ef112
davidclaveau <daveclaveau@gmail.com>
2021-06-24 16:28:55 -0700

    Add sale stauses to sale page using sale model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,15 @@
 class Sale < ActiveRecord::Base
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on < Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
