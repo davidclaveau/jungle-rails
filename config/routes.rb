4bfc3ec5ed69e5a7a87eea1b9cebcf2d8fb8ca15
davidclaveau <daveclaveau@gmail.com>
2021-06-24 12:16:13 -0700

    Add sessions controller, routes, and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
   get '/signup' => 'users#new'
