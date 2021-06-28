8ea1f975a679ca9c1275ecfbeb8af83081fdaeac
davidclaveau <daveclaveau@gmail.com>
2021-06-28 14:51:42 -0700

    Add validation tests to user name, email, and pw

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
   post '/users' => 'users#create'
 
 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,24 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
     
-    it "should successfully create a user" do
-      user = User.create!(:name => "test_name", :email => "test@test.com", :password_digest => "password")
+    it "should successfully create a user (password and confirmation match)" do
+      user = User.create!(:name => "test_name", :email => "test@test.com", :password => "password", :password_confirmation => "password")
       expect(user).to be_present
+    end
+  
+    it ("should provide an error when the password and confirmation don't match") do
+      expect{ User.create!(:name => "test_name", :email => "test@test.com", :password => "password", :password_confirmation => "foo").to raise_error(ActiveRecord::RecordInvalid) }
+    end
+
+    it ("should provide an error when emails match, regardless of case") do
+      user = User.create!(:name => "test_name", :email => "test@test.com", :password => "password", :password_confirmation => "password")
+      expect(user).to be_present
+      
+      expect{ User.create!(:name => "test_name", :email => "TEST@TEST.com", :password => "password", :password_confirmation => "password").to raise_error(ActiveRecord::RecordInvalid) }
+    end
+
+    it ("should provide an error when name is null") do
+      expect{ User.create!(:name => nil, :email => "test@test.com", :password => "password", :password_confirmation => "password").to raise_error(ActiveRecord::RecordInvalid) }
     end
   end
 end
