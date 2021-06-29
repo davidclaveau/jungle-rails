d1d1829f7652e9fce80c02847b0a6bb73ac320da
davidclaveau <daveclaveau@gmail.com>
2021-06-29 11:46:21 -0700

    Refactor product_spec to be easier to read

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,36 +2,49 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
+    before :each do
+      @product = Product.new(
+        name: "Product",
+        price: 350,
+        quantity: 10,
+        category: Category.new(
+          name: "Category"
+        )
+      )
+    end
     
-    # validates :category, presence: true
     it "should successfully create a category" do
-      category = nil
-      expect{ category.products.create!(:name => nil, :price => 350, :quantity => 10).to raise_error(ActiveRecord::RecordInvalid) }
+      category = Category.new(
+        name: "Category"
+      )
+      expect(category).to be_valid
     end
 
-    # validates :name, presence: true
     it "should succeed in creating a product" do
-      category = Category.create!
-      product1 = category.products.create!(:name => "Product", :price => 350, :quantity => 10)
-      expect(product1).to be_present
+      @product.save
+      expect(@product).to be_valid
     end
-      
-    # validates :name, presence: true
-    it "should fail if name doesn't exist" do
-      category = Category.create!
-      expect{ category.products.create!(:name => nil, :price => 350, :quantity => 10).to raise_error(ActiveRecord::RecordInvalid) }
+    
+    it "should fail if product name doesn't exist" do
+      @product.name = nil
+      @product.save
+
+      expect(@product.errors.full_messages[0]).to eq("Name can't be blank")
     end
 
-    # validates :price, presence: true
-    it "should fail if price doesn't exist" do
-      category = Category.create!
-      expect{ category.products.create!(:name => "Product", :price => nil, :quantity => 10).to raise_error(ActiveRecord::RecordInvalid) }
-    end
+    it "should fail if product price isn't a number" do
+      @product.price = "foo"
+      @product.save
 
-    # validates :quantity, presence: true
+      expect(@product.errors.full_messages[0]).to eq("Price is not a number")
+    end
+    
     it "should fail if quantity doesn't exist" do
-      category = Category.create!
-      expect{ category.products.create!(:name => "Product", :price => 350, :quantity => nil).to raise_error(ActiveRecord::RecordInvalid) }
+      @product.quantity = nil
+      @product.save
+
+      expect(@product.errors.full_messages[0]).to eq("Quantity can't be blank")
     end
     
   end
